155d897e8622e45b7b5dab1a1fc205b71ad05478
Oliver Njiru <olivernjiru@gmail.com>
2022-09-12 16:55:38 +0300

    betty

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num < 58)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
