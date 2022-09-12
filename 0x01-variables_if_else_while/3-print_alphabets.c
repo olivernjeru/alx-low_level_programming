6e0d7b401d47250f4c666f4fabdb806ce60d73b4
Oliver Njiru <olivernjiru@gmail.com>
2022-09-12 16:50:59 +0300

    betty

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,18 @@
 int main(void)
 {
     int lw = 'a';
+
     int up = 'A';
+
     while (lw <= 'z')
     {
         putchar(lw);
-        lw += 1;
+        lw++;
     }
     while (up <= 'Z')
     {
         putchar(up);
-        up += 1;
+        up++;
     }
     putchar('\n');
     return (0);
