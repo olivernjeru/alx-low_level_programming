e611e9bac0cbc4c41c0b7b2864f6b0ccd5b8dea3
olivernjeru <olivernjiru@gmail.com>
2022-09-12 05:32:35 -0700

    Number 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: Always 0
+*/
+int main(void)
+{
+char alpha;
+
+for (alpha = 97; alpha < 123; alpha++)
+{
+if (alpha != 101 && alpha != 113)
+{
+putchar(alpha);
+}
+	}
+putchar(n);
+return (0);
+}
