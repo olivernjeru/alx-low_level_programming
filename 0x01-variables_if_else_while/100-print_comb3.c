1c03838207fa94334deba6b63314a8974daf486c
Oliver Njiru <olivernjiru@gmail.com>
2022-09-12 18:02:18 +0300

    betty

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -21,14 +21,14 @@ while (n1 <= 57)
 n2 = n1 + 1;
 while (n2 <= 57)
 {
-    putchar(n1);
-    putchar(n2);
-    if (n1 != 56 || n2 != 57)
-    {
-        putchar(com);
-        putchar(32);
-    }
-    n2 += 1;
+putchar(n1);
+putchar(n2);
+if (n1 != 56 || n2 != 57)
+{
+putchar(com);
+putchar(32);
+}
+n2 += 1;
 }
 n1 += 1;
 }
