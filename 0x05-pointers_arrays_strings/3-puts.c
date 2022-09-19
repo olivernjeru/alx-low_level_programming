31f85fab90726f65b890806272dc839b399d3697
Oliver Njiru <olivernjiru@gmail.com>
2022-09-19 17:08:30 +0300

    added 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to the standard output.
+ * @str: the string to be printed.
+ *
+ * Return: nothing.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
